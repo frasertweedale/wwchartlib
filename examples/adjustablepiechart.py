d58754e972bfa03c6b334db3f47b78ca09811c17
Fraser Tweedale <frasert@jumbolotteries.com>
2011-10-19 14:29:55 +1000

    bug4272 - AdjustablePieChart example: nix timers

--- examples/adjustablepiechart.py
+++ examples/adjustablepiechart.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import sys
-import time
 
 from PySide.QtCore import *
 from PySide.QtGui import *
@@ -9,28 +8,8 @@ from PySide.QtGui import *
 import wwchartlib.piechart
 
 app = QApplication(sys.argv)
-items = [wwchartlib.piechart.PieChartItem(fraction=0.2)] * 4
+items = [wwchartlib.piechart.PieChartItem(fraction=0.2) for x in range(4)]
 chart = wwchartlib.piechart.AdjustablePieChart(items=items)
 chart.show()
-
-
-def _remove():
-    chart.removeChartItem(0)
-
-
-timer1 = QTimer()
-timer1.setSingleShot(True)
-timer1.timeout.connect(_remove)
-
-
-def _add():
-    chart.addChartItem(wwchartlib.piechart.PieChartItem(fraction=0.1))
-    timer1.start(5000)
-
-
-timer = QTimer()
-timer.setSingleShot(True)
-timer.timeout.connect(_add)
-timer.start(5000)
 
 app.exec_()
